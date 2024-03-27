f86376676dfe0c1343aa88cd4bf35601390eafd6
mdizon00 <146342475+mdizon00@users.noreply.github.com>
2024-03-27 12:02:54 +0000

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,52 @@
+/* HALF CAB SOCIETY WEBSITE SCRIPTS */
+
+/* HEADER IN ALL PAGES */
+/* LOGIN MESSAGE */
+var counter = 2;
+function login() {
+    if (counter % 2 == 0) {
+        alert('You have been logged in!');
+    }
+    else {
+        alert('You have been logged out!');
+    }
+    counter += 1;
+    document.getElementById('username').value = "";
+    document.getElementById('password').value = "";
+}
+
+/* HOVER TO PLAY VIDEO, only works for one video
+const video = document.querySelector(".video-container video");
+
+video.addEventListener("mouseenter", () => {
+    video.play();
+});
+
+video.addEventListener("mouseleave", () => {
+    video.pause();
+    video.currentTime = 0;
+});
+*/
+
+/* TRICKS LIST PAGE */
+/* HOVER TO PLAY VIDEO, multiple videos on page */
+document.addEventListener('mouseover', hoverVideo, false);
+var vid = document.getElementsByTagName("video");
+[].forEach.call(vid, function (item) {
+    item.addEventListener('mouseover', hoverVideo, false);
+    item.addEventListener('mouseout', hideVideo, false);
+});
+function hoverVideo(e) {
+    this.play();
+}
+function hideVideo(e) {
+    this.pause();
+    this.currentTime = 0;
+}
+
+/* CONTACT US PAGE */
+/* FORM SEND MESSAGE */
+function send_form(id, content) {
+    var container = document.getElementById(id);
+    container.innerHTML = content;
+}
